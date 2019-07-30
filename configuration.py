34a8685377a93e4d3f686d641784e29a576cc389
NaehaSharif <47408307+NaehaSharif@users.noreply.github.com>
2019-07-30 12:15:18 +0800

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Feb  4 12:23:03 2018
-
-configuration for nneval classify
-
 @author: 22161668
 """
 # classification configuration
 
 
-"""NNeval model and training configurations."""
+"""LCEval model and training configurations."""
 
 class ModelConfig(object):
   """Wrapper class for model hyperparameters."""
@@ -17,15 +13,9 @@ class ModelConfig(object):
   def __init__(self):
     """Sets the default model hyperparameters."""
 
-    # Batch size.
-    #date length =91744
     
     self.val_batch_size = 3976 # size of the validation set 
 
-    # Scale used to initialize model variables.
-     # we can also initialise using random variables
-
-    # LSTM input and output dimensionality, respectively.
     self.sentence_feature_size = 16
     self.nneval_insize= 16
     
@@ -47,7 +37,6 @@ class TrainingConfig(object):
     self.learning_rate = 0.0005
     
     
-# num of examples 137616 where reduced by extracting hodosh examples out
     self.total_examples =132984#155148#177312#88656#132984
     #3058 interations per epoch
     # Optimizer for training the model.
@@ -56,14 +45,3 @@ class TrainingConfig(object):
     self.models_to_save_per_epoch = 1
     self.total_num_epochs = 1000
     
-        # Learning rate for the initial phase of training.
-    
-    
-    
-    
-#    self.learning_rate_decay_factor = 1 
-#    self.num_epochs_per_decay =10 
-    #self.num_epochs_per_decay = 8.0 
-
-    # If not None, clip gradients to this value.
-#    self.clip_gradients = 5.0
