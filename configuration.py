d1aeb7c1a268eac9e0019ea28b73ce4f98935038
NaehaSharif <47408307+NaehaSharif@users.noreply.github.com>
2019-08-22 11:49:10 +0800

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -13,9 +13,6 @@ class ModelConfig(object):
   def __init__(self):
     """Sets the default model hyperparameters."""
 
-    
-    self.val_batch_size = 3976 # size of the validation set 
-
     self.sentence_feature_size = 16
     self.nneval_insize= 16
     
@@ -38,8 +35,6 @@ class TrainingConfig(object):
     
     
     self.total_examples =132984#155148#177312#88656#132984
-    #3058 interations per epoch
-    # Optimizer for training the model.
     self.optimizer = "Adam"
 
     self.models_to_save_per_epoch = 1
